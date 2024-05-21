dedd30d2d2322aa824edfb083c7588b3d90da814
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-05-21 09:51:58 -0400

    add vanier logo

---
+++ VanierLogo.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class VanierLogo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class VanierLogo extends World
+{
+
+    /**
+     * Constructor for objects of class VanierLogo.
+     * 
+     */
+    public VanierLogo()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(900, 473, 1); 
+    }
+}
